0294955fdcd1c076cc73bc93c861ad9bb4df338e
Victor Ottati <victor.ottati@gmail.com>
2020-10-17 18:30:28 +0100

    docs agregado

---
+++ docs/precache-manifest.49f3e447f38c93e5c870a51b5f92c110.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "28e36a852ba92beb6a19b8d3ae3dc75b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "bc50d98d69e6115fba78",
+    "url": "/static/css/main.1beb7d20.chunk.css"
+  },
+  {
+    "revision": "18772ccb525ea2d14384",
+    "url": "/static/js/2.57075067.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/static/js/2.57075067.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bc50d98d69e6115fba78",
+    "url": "/static/js/main.07c2e56e.chunk.js"
+  },
+  {
+    "revision": "daee87f482498cdf724c",
+    "url": "/static/js/runtime-main.29242b1d.js"
+  }
+]);
